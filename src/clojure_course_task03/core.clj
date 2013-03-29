4b960048abcc5c53bde5016ea97326cb27da9d73
Sergey Shishmintzev <sergey.shishmintzev@gmail.com>
2013-03-29 23:29:49 +0200

    Optimization suggested by @nbeloglazov

--- src/clojure_course_task03/core.clj
+++ src/clojure_course_task03/core.clj
@@ -34,7 +34,7 @@
      (and (= (first flds) :all) (= (first allowed) :all)) "*"
      (and (= (first flds) :all) (not= (first allowed) :all)) (-fields** allowed)
      (= :all (first allowed)) (-fields** flds)
-     :else (-fields** (->> flds (map v) (filter (comp not nil?)))))))
+     :else (-fields** (filter v flds)))))
 
 (defn select* [table-name {:keys [fields where join order limit offset]}]
   (-> (str "SELECT " fields " FROM " table-name " ")
